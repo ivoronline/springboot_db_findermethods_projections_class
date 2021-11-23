4f71695f6610a44adb625ecb0f5aee38d34df49a
ivoronline <ivoronline@gmail.com>
2021-11-23 22:53:49 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_db_findermethods_projections_class/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_db_findermethods_projections_class/controllers/MyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
+  //PROPERTIES
   @Autowired PersonRepository personRepository;
 
   //================================================================
@@ -16,8 +17,18 @@ public class MyController {
   //================================================================
   @RequestMapping("FindDTOByName")
   PersonDTO findDTOByName() {
+
+    //GET DTO FROM DB
     PersonDTO personDTO = personRepository.findDTOByName("John");
+
+    //GET PROJECTION PROPERTIES
+    String  name = personDTO.name;
+    Integer age  = personDTO.age;
+    System.out.println(name + " is " + age + " years old");
+
+    //RETURN DTO
     return    personDTO;
+
   }
 
 }
